1f0225fa66d646ec77ba4c0649f40c9bf60f7b2d
Alaa Saleem <alaa.bzu@gmail.com>
2023-12-22 09:49:36 +0200

    Polynomial Regrission

--- polynomialRegrission.py
+++ polynomialRegrission.py
@@ -21,76 +21,114 @@ val_errors = []
 best_degree = None
 best_val_error = float('inf')
 
-# Loop on degrees of the polynomial from 1 to 10
 for degree in range(1, 11):
-    # Add a feature to know that it is a polynomial regression with a degree
     poly_reg = PolynomialFeatures(degree=degree)
-    
-    # Use the training set to transform it to new features by using the polynomial transformation on the original features (x, x^2, x^3, etc)
     X_poly_train = poly_reg.fit_transform(X_train)
-    
-    # Use the transform function to make the validation set look like the new features to be ready to predict
     X_poly_val = poly_reg.transform(X_val)
-
-    # Create a Linear Regression object to apply the new features (which are polynomial and non-linear data that have a certain degree)
-    poly_model = LinearRegression()
     
-    # Train the model
+    poly_model = LinearRegression()
     poly_model.fit(X_poly_train, y_train)
 
-    # Predict the y values using the polynomial model
     y_train_pred = poly_model.predict(X_poly_train)
     y_val_pred = poly_model.predict(X_poly_val)
     
-    # Find the mean squared error (MSE)
     train_error = mean_squared_error(y_train, y_train_pred)
     val_error = mean_squared_error(y_val, y_val_pred)
 
     train_errors.append(train_error)
     val_errors.append(val_error)
 
-    # Check for the best fitting model among all degrees
     if val_error < best_val_error:
         best_val_error = val_error
         best_degree = degree
 
-    # Plotting the surface for the current degree
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(projection='3d')
 
-    # Generate meshgrid for the surface plot
+    examples = [
+        (X_train, y_train, 'red', 'Training Examples'),
+        (X_val, y_val, 'green', 'Validation Examples'),
+    ]
+
+    for X, y, color, label in examples:
+        ax.scatter(X[:, 0], X[:, 1], y, c=color, label=label, marker='o', s=50, alpha=0.8)
+
+    # Generating the polynomial curve
     x1, x2 = np.meshgrid(np.linspace(X_train[:, 0].min(), X_train[:, 0].max(), 100),
                          np.linspace(X_train[:, 1].min(), X_train[:, 1].max(), 100))
     X_mesh = np.c_[x1.ravel(), x2.ravel()]
-    X_mesh_poly = PolynomialFeatures(degree=degree).fit_transform(X_mesh)
+    X_mesh_poly = poly_reg.transform(X_mesh)
     y_mesh = poly_model.predict(X_mesh_poly).reshape(x1.shape)
-
-    # Plotting the surface
-    ax.plot_surface(x1, x2, y_mesh, alpha=0.5)
-
-    # Plot training and validation examples
-    ax.scatter(X_train[:, 0], X_train[:, 1], y_train, color='red', label='Training Examples')
-    ax.scatter(X_val[:, 0], X_val[:, 1], y_val, color='green', label='Validation Examples')
-
-    ax.set_xlabel('x1')
-    ax.set_ylabel('x2')
-    ax.set_zlabel('y')
-    ax.set_title(f'Degree {degree} Polynomial')
-
-    # Plotting training and validation errors on the plot
-    ax.text2D(0.05, 0.95, f'Train Error: {train_error:.4f}\nValidation Error: {val_error:.4f}', transform=ax.transAxes)
     
+    ax.plot_surface(x1, x2, y_mesh, alpha=0.5, color='blue')  # Plotting the polynomial curve
+
+    ax.set_xlabel('x1', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_ylabel('x2', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_zlabel('y', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_title(f'Degree {degree} Polynomial', fontdict={'fontname': 'Times New Roman', 'fontsize': 27, 'color': 'white'})
+
+    legend = ax.legend(bbox_to_anchor=(0.05, 0.6), fontsize=12, facecolor='black', edgecolor='white',
+                   prop={'family': 'Times New Roman', 'weight': 'bold'})
+
+    for text in legend.get_texts(): 
+        text.set_color('white')
+
+    ax.set_facecolor('black')
+    fig.patch.set_facecolor('black')
+
+    ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+
+    for axis in [ax.xaxis, ax.yaxis, ax.zaxis]:
+        axis._axinfo['grid'].update(color='white')
+        [tick.set(fontname="Times New Roman", fontsize=12, color='white') for tick in axis.get_ticklabels()]
+
     comment = ""
-    if (train_error < 0.5 and val_error > 0.5 ):
-        comment = "**Overfitting**: Low train error, high validation error. Model is too complex and captures noise in data."
-    elif (train_error > 0.5 and val_error > 0.5 ):
-        comment = "**Underfitting**: High train & validation error. Model is too simple and cannot capture the complexity of data."
+    if (train_error < 0.5 and val_error > 0.5):
+        comment = "**Overfitting**: Low train error, high validation error. Model captures noise in data."
+    elif (train_error > 0.5 and val_error > 0.5):
+        comment = "**Underfitting**: High train & validation error. Model is too simple."
     else:
-        comment = "**Good Fitting**: Model generalizes well."
+        comment = "**Good Fitting**: Low train & validation error. Model generalizes well."
 
-    ax.text2D(0.05, 0.85, comment, color='green' if train_error < 0.5 and val_error < 0.5 else 'red', transform=ax.transAxes, fontweight='bold')
+    ax.text2D(0.05, 0.85, comment, color='green' if train_error < 0.5 and val_error < 0.5 else 'red',
+          transform=ax.transAxes, fontweight='bold', fontsize=12, fontname='Times New Roman')
 
-    plt.legend()
+    ax.text2D(0.35, 0.88, f'Training Error: {train_error:.4f}\nValidation Error: {val_error:.4f}',
+              transform=ax.transAxes, fontweight='bold', fontsize=12, fontname='Times New Roman', color='white')
+    
     plt.show()
 
 print(f"The best fitting model is Degree {best_degree} Polynomial with a validation error of {best_val_error:.4f}")
+
+# Plotting validation error vs polynomial degree curve
+degrees = range(1, 11)
+# Find the index of the minimum validation error
+min_val_error_idx = np.argmin(val_errors)
+min_val_error_degree = degrees[min_val_error_idx]
+min_val_error = val_errors[min_val_error_idx]
+
+plt.figure(figsize=(8, 6), facecolor='black')
+plt.plot(degrees, val_errors, marker='o', linestyle='-', color='red')
+plt.scatter(min_val_error_degree, min_val_error, color='green', s=100, label=f'Lowest Error ({min_val_error:.4f})')
+
+plt.annotate(f'Minimum Error: Degree {min_val_error_degree}', 
+             xy=(min_val_error_degree, min_val_error), 
+             xytext=(min_val_error_degree - 1.5, min_val_error + 0.1), 
+             arrowprops=dict(color='white', arrowstyle='->', connectionstyle='arc3,rad=.5'),
+             fontsize=12, fontname='Times New Roman', color='white')
+
+plt.title('Validation Error vs Polynomial Degree', fontdict={'fontname': 'Times New Roman', 'fontsize': 18, 'color': 'white'})
+plt.xlabel('Polynomial Degree', fontdict={'fontname': 'Times New Roman', 'fontsize': 14, 'color': 'white'})
+plt.ylabel('Validation Error', fontdict={'fontname': 'Times New Roman', 'fontsize': 14, 'color': 'white'})
+plt.xticks(degrees, color='white', fontname='Times New Roman')
+plt.yticks(color='white', fontname='Times New Roman')
+plt.grid(True, color='white')
+plt.gca().set_facecolor('black')
+
+legend = plt.legend(loc='upper right', fontsize=12, facecolor='black', edgecolor='white', prop={'family': 'Times New Roman', 'weight': 'bold'})
+for text in legend.get_texts(): 
+    text.set_color('white')
+
+plt.show()
--- scatterPlot3D.py
+++ scatterPlot3D.py
@@ -1,58 +1,115 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
-# Read the data from the csv file
+# Load the dataset
 dataFrame = pd.read_csv("C:\\Users\\User\\OneDrive\\BZU\\120-07\\ML\\Assignment2\\Hyperparameter-Tuning\\data_reg.csv")
 X = dataFrame[['x1', 'x2']].values  # Features x1 and x2
 y = dataFrame['y'].values  # Target variable y
 
-# Split data into training set (the first 120 examples), validation set (the next 40 examples), and testing set (the last 40 examples)
-X_main, X_test, y_main, y_test = train_test_split(X, y, test_size=40, shuffle=False) #split the data set into main (160 examples) and test (last 40 examples)
-X_train, X_val, y_train, y_val = train_test_split(X_main, y_main, test_size=40, shuffle=False) # split the main (160 examples) into training (first 120 examples) and validation (last 40 examples)
+# Split data into training set (first 120 examples), validation set (next 40 examples), and testing set (last 40 examples)
+X_main, X_test, y_main, y_test = train_test_split(X, y, test_size=40, shuffle=False)
+X_train, X_val, y_train, y_val = train_test_split(X_main, y_main, test_size=40, shuffle=False)
 
+train_errors = []
+val_errors = []
 
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(projection='3d')
+best_degree = None
+best_val_error = float('inf')
 
-examples = [
-    (X_train, y_train, '#1f77b4', f'Training Set ({len(X_train)} examples)'),  # Training set examples
-    (X_val, y_val, '#ff7f0e', f'Validation Set ({len(X_val)} examples)'),  # Validation set examples
-    (X_test, y_test, '#2ca02c', f'Test Set ({len(X_test)} examples)')  # Test set examples
-]
-
-# Plot each set as a scatter plot with their colors, labels
-for X, y, color, label in examples:
-    ax.scatter(X[:, 0], X[:, 1], y, c=color, label=label, marker='o', s=50, alpha=0.8)
+for degree in range(1, 11):
+    poly_reg = PolynomialFeatures(degree=degree)
+    X_poly_train = poly_reg.fit_transform(X_train)
+    X_poly_val = poly_reg.transform(X_val)
     
-# Define the font style
-font = {'fontname': 'Times New Roman', 'fontsize': 12, 'color': 'white'} 
+    poly_model = LinearRegression()
+    poly_model.fit(X_poly_train, y_train)
 
-# Set labels for x, y, and z axes
-ax.set_xlabel('x1',**font) 
-ax.set_ylabel('x2',**font) 
-ax.set_zlabel('y',**font)  
+    y_train_pred = poly_model.predict(X_poly_train)
+    y_val_pred = poly_model.predict(X_poly_val)
+    
+    train_error = mean_squared_error(y_train, y_train_pred)
+    val_error = mean_squared_error(y_val, y_val_pred)
 
-# Add legend with the updated labels including the number of examples
-legend = ax.legend(bbox_to_anchor=(0.05, 0.6), fontsize=12, facecolor='black', edgecolor='white',
-                   prop={'family': 'Times New Roman', 'weight': 'bold'})
+    train_errors.append(train_error)
+    val_errors.append(val_error)
 
-for text in legend.get_texts(): 
-    text.set_color('white')
+    if val_error < best_val_error:
+        best_val_error = val_error
+        best_degree = degree
 
-ax.set_facecolor('black') 
-fig.patch.set_facecolor('black')  
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(projection='3d')
 
-ax.set_title('Scatter Plot of Training, Validation, and Test Sets', y=1.02,
-             fontdict={'fontname': 'Times New Roman', 'fontsize': 27, 'color': 'white'}) 
+    examples = [
+        (X_train, y_train, 'red', 'Training Examples'),
+        (X_val, y_val, 'green', 'Validation Examples'),
+    ]
 
-# Setting the surface color to black
-ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))  
-ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0)) 
-ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    for X, y, color, label in examples:
+        ax.scatter(X[:, 0], X[:, 1], y, c=color, label=label, marker='o', s=50, alpha=0.8)
 
-for axis in [ax.xaxis, ax.yaxis, ax.zaxis]:
-    axis._axinfo['grid'].update(color='white') 
-    [tick.set(fontname="Times New Roman", fontsize=12, color='white') for tick in axis.get_ticklabels()] 
+    # Generating the polynomial curve
+    x1, x2 = np.meshgrid(np.linspace(X_train[:, 0].min(), X_train[:, 0].max(), 100),
+                         np.linspace(X_train[:, 1].min(), X_train[:, 1].max(), 100))
+    X_mesh = np.c_[x1.ravel(), x2.ravel()]
+    X_mesh_poly = poly_reg.transform(X_mesh)
+    y_mesh = poly_model.predict(X_mesh_poly).reshape(x1.shape)
+    
+    ax.plot_surface(x1, x2, y_mesh, alpha=0.5, color='blue')  # Plotting the polynomial curve
 
+    ax.set_xlabel('x1', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_ylabel('x2', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_zlabel('y', fontsize=12, fontname='Times New Roman', color='white')
+    ax.set_title(f'Degree {degree} Polynomial', fontdict={'fontname': 'Times New Roman', 'fontsize': 27, 'color': 'white'})
+
+    legend = ax.legend(fontsize=12, facecolor='black', edgecolor='white', prop={'family': 'Times New Roman', 'weight': 'bold'})
+    for text in legend.get_texts():
+        text.set_color('white')
+
+    ax.set_facecolor('black')
+    fig.patch.set_facecolor('black')
+
+    ax.w_xaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    ax.w_yaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+    ax.w_zaxis.set_pane_color((0.0, 0.0, 0.0, 1.0))
+
+    for axis in [ax.xaxis, ax.yaxis, ax.zaxis]:
+        axis._axinfo['grid'].update(color='white')
+        [tick.set(fontname="Times New Roman", fontsize=12, color='white') for tick in axis.get_ticklabels()]
+
+    comment = ""
+    if (train_error < 0.5 and val_error > 0.5):
+        comment = "**Overfitting**: Low train error, high validation error. Model captures noise in data."
+    elif (train_error > 0.5 and val_error > 0.5):
+        comment = "**Underfitting**: High train & validation error. Model is too simple."
+    else:
+        comment = "**Good Fitting**: Model generalizes well."
+
+    ax.text2D(0.05, 0.85, comment, color='green' if train_error < 0.5 and val_error < 0.5 else 'red',
+          transform=ax.transAxes, fontweight='bold', fontsize=12, fontname='Times New Roman')
+
+    ax.text2D(0.05, 0.75, f'Training Error: {train_error:.4f}\nValidation Error: {val_error:.4f}',
+              transform=ax.transAxes, fontweight='bold', fontsize=12, fontname='Times New Roman', color='white')
+    
+    plt.show()
+
+print(f"The best fitting model is Degree {best_degree} Polynomial with a validation error of {best_val_error:.4f}")
+
+# Plotting validation error vs polynomial degree curve
+degrees = range(1, 11)
+
+plt.figure(figsize=(8, 6), facecolor='black')
+plt.plot(degrees, val_errors, marker='o', linestyle='-', color='red')
+plt.title('Validation Error vs Polynomial Degree', fontdict={'fontname': 'Times New Roman', 'fontsize': 18, 'color': 'white'})
+plt.xlabel('Polynomial Degree', fontdict={'fontname': 'Times New Roman', 'fontsize': 14, 'color': 'white'})
+plt.ylabel('Validation Error', fontdict={'fontname': 'Times New Roman', 'fontsize': 14, 'color': 'white'})
+plt.xticks(degrees, color='white', fontname='Times New Roman')
+plt.yticks(color='white', fontname='Times New Roman')
+plt.grid(True, color='white')
+plt.gca().set_facecolor('black')
 plt.show()
